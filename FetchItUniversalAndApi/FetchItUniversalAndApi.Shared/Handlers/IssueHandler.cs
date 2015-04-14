d31b4196170cc3cb2f66477f906abc16b934dd7b
jakub9 <jakub.krzysztof.czapski@gmail.com>
2015-04-14 18:13:26 +0200

    updated Create() Search() Disable() and Delete() methods in IssueHandler. I'm getting some very strange error in the project concerning AnimationMetrics.

--- FetchItUniversalAndApi/FetchItUniversalAndApi.Shared/Handlers/IssueHandler.cs
+++ FetchItUniversalAndApi/FetchItUniversalAndApi.Shared/Handlers/IssueHandler.cs
@@ -1,30 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Windows.UI.Core.AnimationMetrics;
 using FetchItUniversalAndApi.Handlers.Interfaces;
+using FetchItUniversalAndApi.Models;
 
 namespace FetchItUniversalAndApi.Handlers
 {
     class IssueHandler: ICreate, IDelete, IDisable, ISearch, ISuspend, IUpdate
     {
+        private IssueModel _newIssue;
+        private IssueModel _selectedIssue;
+        private IssueHandler _handler;
+        private IEnumerable<IssueModel> _currentIssues;
+
+        public IEnumerable<IssueModel> CurrentIssues
+        {
+            get { return _currentIssues; }
+            set { _currentIssues = value; }
+        }
+
+        public IssueHandler Handler
+        {
+            get { return _handler; }
+            set { _handler = value; }
+        }
+
+        public IssueModel NewIssue
+        {
+            get { return _newIssue; }
+            set { _newIssue = value; }
+        }
+
+        public IssueModel SelectedIssue
+        {
+            get { return _selectedIssue; }
+            set { _selectedIssue = value; }
+        }
+
         public void Create(object obj)
         {
-            throw new NotImplementedException();
+            _handler.Create(_newIssue);
         }
 
         public void Delete(object obj)
         {
-            throw new NotImplementedException();
+            _handler.Delete(_selectedIssue);
         }
 
         public void Disable(object obj)
         {
-            throw new NotImplementedException();
+            _handler.Disable(_selectedIssue);
         }
 
         public IEnumerable<object> Search(object obj)
         {
-            throw new NotImplementedException();
+            string userinput = "test input";
+
+            foreach (IssueModel issueModel in _currentIssues.Where(issue => issue.IssueTitle.Contains(userinput)))
+            {
+                return issueModel.IssueDetails;
+            }
+            return _currentIssues;
         }
 
         public void Suspend(object obj)
