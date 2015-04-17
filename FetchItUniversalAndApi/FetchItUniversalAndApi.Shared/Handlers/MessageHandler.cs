9f0d0973b50ddec2a8f8620c59af09f00aa2e88a
Lalli-Oni <lalli.oni@gmail.com>
2015-04-17 12:53:32 +0200

    CreateFeedback():
    Working, might need some checks and exceptions.
    
    GetFeedback():
    Working, currently gets all Feedbacks according to Status

--- FetchItUniversalAndApi/FetchItUniversalAndApi.Shared/Handlers/MessageHandler.cs
+++ FetchItUniversalAndApi/FetchItUniversalAndApi.Shared/Handlers/MessageHandler.cs
@@ -1,24 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using Windows.Data.Json;
+using FetchItUniversalAndApi.Handlers.Interfaces;
 using FetchItUniversalAndApi.Models;
 
 namespace FetchItUniversalAndApi.Handlers
 {
-
+    //Author: Lárus Þór Jóhannsson
     /// <summary>
     /// Handles getting and creating messages sent to users.
     /// </summary>
     static class MessageHandler
     {
+    #region Enums
+        public enum FeedbackStatus
+        {
+            DeleteDeleted = 2,
+            Suspended = 3,
+            Disabled = 4,
+            Active = 5,
+            Unactivated = 6
+        }
+    #endregion
+
+    #region Fields and Properties
         //The httpclient should probably be one object that all handlers call upon. There "shouldn't" be any reason to dispose of it or flush it.
         //But we should keep an eye out for if it's not closing the connections or not dumping the resources.
         private static HttpClient msgWebClient = new HttpClient();
         //TODO: This could probably be somewhere better and globally available. Also needs to be changed to refer to the intended server.
-        private static string serverLocation = "http://localhost:1337";
+        private static readonly string serverLocation = "http://fetchit.mortentoudahl.dk/api/";
+    #endregion
+
+    #region Methods
+        #region Constructor
+        static MessageHandler()
+        {
+            msgWebClient.BaseAddress = new Uri(serverLocation);
+            msgWebClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        #endregion
 
         /// <summary>
         /// Creates a FeedbackModel object and sends it to the database server using POST by parsing it to json.
@@ -27,15 +52,16 @@ namespace FetchItUniversalAndApi.Handlers
         /// The rating is not optional and must be a number between 1 and 10. It represents how the Taskmaster values the service that the Fetcher provided. 
         /// <param name="optionalText"></param>
         /// OptionalText is optional. Describes in more detail the service the Taskmaster provided.
-        public static async void CreateFeedback(int rating, string optionalText)
+        public static async void CreateFeedback(int rating, string optionalText, TaskModel fromTask)
         {
             if (rating < 1 || rating > 10)
             {
                 throw new Exception("Rating is out of bounds. Must be between 1 and 10. Contact Þór for free cookies and milk for discovering the bug.");
             }
-            msgWebClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             FeedbackModel createdFeedback = new FeedbackModel();
             createdFeedback.FeedbackComment = optionalText;
+            createdFeedback.FK_FeedbackForTask = fromTask.TaskId;
+            createdFeedback.FK_FeedbackStatus = (int) FeedbackStatus.Active;
             try
             {
                 createdFeedback.FeedbackRating = (byte)rating;
@@ -48,7 +74,7 @@ namespace FetchItUniversalAndApi.Handlers
             //TODO: The object being PUT needs to be formatted so that the webApi can put it in the right database.
             try
             {
-                msgWebClient.PostAsJsonAsync(serverLocation, createdFeedback);
+                msgWebClient.PostAsJsonAsync("FeedbackModels", createdFeedback);
             }
             catch (Exception)
             {
@@ -56,9 +82,22 @@ namespace FetchItUniversalAndApi.Handlers
             }
         }
 
-        public static IEnumerable<FeedbackModel> GetFeedback()
+        /// <summary>
+        /// A method that returns a collection of all Feedback objects
+        /// </summary>
+        /// <returns> A IENumerable collection of Feedback objects</returns>
+        public static IEnumerable<FeedbackModel> GetFeedback(FeedbackStatus status)
         {
-            throw new NotImplementedException();
+                try
+                {
+                    var reports = Task.Run(async () => await msgWebClient.GetAsync("FeedbackModels"));
+                    return reports.Result.Content.ReadAsAsync<IEnumerable<FeedbackModel>>().Result;
+                }
+                catch (Exception exception)
+                {
+                    throw exception;
+                }
+            
         }
 
         public static void CreateTaskComment(TaskModel taskObj)
@@ -66,7 +105,7 @@ namespace FetchItUniversalAndApi.Handlers
             throw new NotImplementedException();
         }
 
-        public static IEnumerable<CommentModel> GetTAskComments(TaskModel fromTask)
+        public static IEnumerable<CommentModel> GetTaskComments(TaskModel fromTask)
         {
             throw new NotImplementedException();
         }
@@ -81,9 +120,10 @@ namespace FetchItUniversalAndApi.Handlers
             throw new NotImplementedException();
         }
 
-        public static IEnumerable<NotificationModel> GetNoticications()
+        public static IEnumerable<NotificationModel> GetNotifications()
         {
             throw new NotImplementedException();
         }
+    #endregion
     }
 }
--- FetchItUniversalAndApi/FetchItUniversalAndApi.Windows/MainPage.xaml.cs
+++ FetchItUniversalAndApi/FetchItUniversalAndApi.Windows/MainPage.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,6 +18,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using FetchItUniversalAndApi.Handlers;
+using FetchItUniversalAndApi.Models;
 
 namespace FetchItUniversalAndApi
 {
@@ -22,9 +28,30 @@ namespace FetchItUniversalAndApi
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private static HttpClient msgWebClient = new HttpClient();
+        private static readonly string serverLocation = "http://fetchit.mortentoudahl.dk/api/";
+        private int rating = 3;
+        private string comment = "lol, this sucks";
+        private IEnumerable<TaskModel> taskList;
         public MainPage()
         {
             this.InitializeComponent();
+            msgWebClient.BaseAddress = new Uri(serverLocation);
+            msgWebClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            try
+            {
+                    var reports = Task.Run(async () => await msgWebClient.GetAsync("TaskModels"));
+                    taskList = reports.Result.Content.ReadAsAsync<IEnumerable<TaskModel>>().Result;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+            TaskModel testTask = taskList.ElementAt(1);
+            MessageHandler.CreateFeedback(rating, comment, testTask);
+            IEnumerable<FeedbackModel> testFeedbacks = MessageHandler.GetFeedback(MessageHandler.FeedbackStatus.Active);
+            List<FeedbackModel> testFeedbackList = testFeedbacks.ToList();
         }
     }
 }
