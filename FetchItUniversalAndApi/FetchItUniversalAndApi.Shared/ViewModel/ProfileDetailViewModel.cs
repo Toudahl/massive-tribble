9087cab8865d5c5b7858581d46e377dc6e6ad4b9
jakub9 <jakub.krzysztof.czapski@gmail.com>
2015-05-08 11:06:57 +0200

    workin on ProfileDetailPage

--- FetchItUniversalAndApi/FetchItUniversalAndApi.Shared/ViewModel/ProfileDetailViewModel.cs
+++ FetchItUniversalAndApi/FetchItUniversalAndApi.Shared/ViewModel/ProfileDetailViewModel.cs
@@ -2,27 +2,38 @@
 using System.Collections.Generic;
 using System.Text;
 using FetchItUniversalAndApi.Handlers;
+using FetchItUniversalAndApi.Models;
 
 
 namespace FetchItUniversalAndApi.ViewModel
 {
     class ProfileDetailViewModel
     {
-        public ProfileHandler Ph { get; set; }
+        public ProfileHandler ProfileHandler { get; set; }
         public string selectedprofileName { get; set; }
         public string selectedprofileMobile { get; set; }
         public string selectedprofileAddress { get; set; }
         public string selectedprofileEmail { get; set; }
         public int selectedprofileId { get; set; }
+        public int selectedprofileRating { get; set; }
 
+       
 
         public ProfileDetailViewModel()
         {
-            selectedprofileName = Ph.SelectedProfile.ProfileName;
-            selectedprofileMobile = Ph.SelectedProfile.ProfileMobile;
-            selectedprofileEmail = Ph.SelectedProfile.ProfileEmail;
-            selectedprofileAddress = Ph.SelectedProfile.ProfileAddress;
-            selectedprofileId = Ph.SelectedProfile.ProfileId;
+            ProfileHandler = ProfileHandler.GetInstance();
+            
+            if (ProfileHandler.SelectedProfile!=null)
+            {
+            selectedprofileName = ProfileHandler.SelectedProfile.ProfileName;
+            selectedprofileMobile = ProfileHandler.SelectedProfile.ProfileMobile;
+            selectedprofileEmail = ProfileHandler.SelectedProfile.ProfileEmail;
+            selectedprofileAddress = ProfileHandler.SelectedProfile.ProfileAddress;
+            selectedprofileId = ProfileHandler.SelectedProfile.ProfileId;  
+            
+            }
+            
+            
 
 
 
