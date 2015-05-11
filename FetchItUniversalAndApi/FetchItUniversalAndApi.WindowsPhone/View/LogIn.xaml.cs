190ec940d0e100b7dbcf11430780a094dbd6b7f0
Toudahl <toudahl@protonmail.ch>
2015-05-11 12:26:21 +0200

    Added views to the windows phone project
    Prevents errors in the error list.

--- FetchItUniversalAndApi/FetchItUniversalAndApi.WindowsPhone/View/LogIn.xaml.cs
+++ FetchItUniversalAndApi/FetchItUniversalAndApi.WindowsPhone/View/LogIn.xaml.cs
@@ -1,45 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using FetchItUniversalAndApi.Handlers;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace FetchItUniversalAndApi
+namespace FetchItUniversalAndApi.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ProfileHandler ph;
         public MainPage()
         {
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
+            ph = ProfileHandler.GetInstance();
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private void RegisterButton_OnClick(object sender, RoutedEventArgs e)
         {
-            // TODO: Prepare page for display here.
+            this.Frame.Navigate(typeof(RegisterUser));
+        }
 
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
+        private async void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(1500);
+            if (ph.CurrentLoggedInProfile != null)
+            {
+                Frame.Navigate(typeof(LandingPage));
+            }
         }
     }
 }
