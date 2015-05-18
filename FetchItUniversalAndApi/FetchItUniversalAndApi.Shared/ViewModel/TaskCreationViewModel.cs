f84b733608b046fb6ae9ebb7601379c179c63d59
Lalli-Oni <lalli.oni@gmail.com>
2015-05-18 22:16:29 +0200

    Assets:
    Added basic .png assets
    
    NullToPictureConverter:
    A converter for the NotificationRead property so it shows ExclamationMark if not read
    
    LandingPage:
    Drastic UI changes. Seeing is believing

--- FetchItUniversalAndApi/FetchItUniversalAndApi.Shared/ViewModel/TaskCreationViewModel.cs
+++ FetchItUniversalAndApi/FetchItUniversalAndApi.Shared/ViewModel/TaskCreationViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Windows.Input;
 using Windows.UI.Popups;
